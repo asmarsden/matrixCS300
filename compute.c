e95da154558d771ce10de41493e553cd2d0edbf5
asmarsden <asmarsden@crimson.ua.edu>
2019-11-16 20:15:37 -0500

    produces correct output

--- compute.c
+++ compute.c
@@ -10,7 +10,7 @@
 #include <pthread.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-
+pthread_mutex_t lock;
 typedef struct QueueMessage{
     long type;
     int jobid;
@@ -20,69 +20,71 @@ typedef struct QueueMessage{
     int data[100];
 } Msg;
 
-void * calculate(){
+void * calculate(){//still need to figure out what this should return
+
 //grab message type 1
-    printf("debug compute 5\n");
+    //printf("debug compute 5\n");
 
 key_t key;
 Msg msg;
     key = ftok("/home/asmarsden",11696847);//use asmarsden??
-    printf("debug compute 6\n");
-//TODO: mathy math and locks
+    //printf("debug compute 6\n");
+//TODO: mathy math and locks. i think this is done
 int msgid = msgget(key, 0666 | IPC_CREAT);
-msgrcv(msgid, &msg, 7*sizeof(int), 1, 0);//check tha tthis is right
-printf("recieved message\n");
-printf("message info: type: %ld\n", msg.type);
-printf("message info: jobid: %d\n", msg.jobid);
-printf("message info: rowvec: %d\n", msg.rowvec);
-printf("message info: colvec: %d\n", msg.colvec);
-printf("message info: innerdim: %d\n", msg.innerDim);
-printf("message info: data[0]: %d\n", msg.data[0]);
+pthread_mutex_lock(&lock);
+msgrcv(msgid, &msg, 106*sizeof(int), 1, 0);//check tha tthis is right
+//printf("recieved message\n");
+//printf("message info: type: %ld\n", msg.type);
+//printf("message info: jobid: %d\n", msg.jobid);
+//printf("message info: rowvec: %d\n", msg.rowvec);
+//printf("message info: colvec: %d\n", msg.colvec);
+//printf("message info: innerdim: %d\n", msg.innerDim);
+//printf("message info: data[0]: %d\n", msg.data[0]);
 //lock
-    printf("debug compute 7\n");
-
-msg.type = 2;
-msg.jobid = 2*msg.jobid;
-msg.rowvec = 2*msg.rowvec;
-msg.colvec = 2*msg.colvec;
-msg.innerDim = 2*msg.innerDim;
-msg.data[0] = 2*msg.data[0];
-msgsnd(msgid, &msg, 7*sizeof(int),2);//make sure this is right
+    //printf("debug compute 7\n");
+Msg returnmsg;
+returnmsg.type = 2;
+returnmsg.rowvec = msg.rowvec;
+returnmsg.colvec = msg.colvec;
+returnmsg.jobid = msg.jobid;
+returnmsg.innerDim = msg.innerDim;
+int sum = 0;
+for (int i = 0; i < msg.innerDim * 2; i++){
+    printf("data[%d] = %d\n",i, msg.data[i]); 
+}
+for (int i = 0; i < msg.innerDim; i++){
+    sum += msg.data[i] * msg.data[i+msg.innerDim];
+    //printf("sum += %d * %d to = %d\n", msg.data[i], msg.data[i+msg.innerDim], sum);
+}
+//printf("sum for %d,%d is %d\n", msg.colvec, msg.rowvec, sum);
+returnmsg.data[0] = sum;
+msgsnd(msgid, &returnmsg, 7*sizeof(int),2);//make sure this is right
 //unlock
-
-//int sum = 0;
-//lock. might have to lock before this?
-//for (int i = 0; i < rowvec.size; i++){
-//    sum += rowvec[i]*colvec[i];
-//}
-//package sum and send to package
-//print sent successful
-//unlock
-    printf("debug compute 8\n");
-
+pthread_mutex_unlock(&lock);
+//printf("debug compute 8\n");
 return NULL;
 }
 
 int main(){ 
     int numThreads = 0;
-    printf("debug compute 1\n");
+  //  printf("debug compute 1\n");
     pthread_t p[100];//is this a good number?
-    pthread_attr_t attr;
+    //pthread_attr_t attr;
     while(1){numThreads++;
-        printf("debug compute 2\n");
+    //    printf("debug compute 2\n");
         //calculate();
         //make threads
         pthread_create(&p[0], NULL, calculate, NULL);//figure out if those nulls should be there
 //im overcomplicating this
 if (numThreads > 100) break;
     }
-        printf("debug compute3\n");
+      //  printf("debug compute3\n");
 
 while (numThreads>0){
     pthread_join(p[numThreads-1], NULL);
     numThreads--;
 }
-    printf("debug compute 4\n");
+    //printf("debug compute 4\n");
 
 
 return 0;
--- package.c
+++ package.c
@@ -15,6 +15,15 @@
 //ipcs -q -o to check message queue
 //ipcrm -q (number) to remove message from queue
 
+/*right now the biggest thing i want to do
+is figure out how compute.c should know
+the size of the thing that will be sent off
+and read in*/
+
+//nothing should be hardcoded, so i gotta fix those
+
+
+
 //usage:
 //./package matrix1 matrix2 result secs between thread creation
 #include <stdio.h>
@@ -44,74 +53,97 @@ int populate(int row, int col, int inner, int data[], int id){
     msg.rowvec=row;
     msg.colvec=col;
     msg.innerDim =inner;
-    msg.data=data; //have to figure out how big to make size...
-    msgsnd(msgid, &msg, 7*sizeof(int), 1);//make sure this is right
-    printf("sent message %d\n", x);
+    for (int i = 0; i < inner * 2; i++){
+        msg.data[i]=data[i];
+        printf("adding %d to dot product of %d\n", data[i], id);
+    }
+    
+    //msg.data=data; //have to figure out how big to make size...
+    msgsnd(msgid, &msg, ((inner*2)+6)*sizeof(int), 1);//make sure this is right
+    //printf("sent message %d\n", x);
     return 0;
 }
 
-void recieve(int* result){
+void recieve(int** result){
 key_t key;
 Msg msg;
     key = ftok("/home/asmarsden",11696847);//use asmarsden??
 int msgid = msgget(key, 0666 | IPC_CREAT);
 msgrcv(msgid, &msg, 7*sizeof(int), 2, 0);
-result[msg.row][msg.col]=msg.data[0];//passing by reference
-printf("recieved message. info:\n");
-printf("type: %ld\njobid: %d\n rowvec:%d\n", msg.type, msg.jobid, msg.rowvec);
-printf("colvec: %d\ninnerDim: %d\n, data[0]: %d\n", msg.colvec, msg.innerDim, msg.data[0]);
+result[msg.rowvec][msg.colvec]=msg.data[0];//passing by reference
+//printf("recieved message. info:\n");
+//printf("type: %ld\njobid: %d\n rowvec:%d\n", msg.type, msg.jobid, msg.rowvec);
+//printf("colvec: %d\ninnerDim: %d\n, data[0]: %d\n", msg.colvec, msg.innerDim, msg.data[0]);
 return;
 }
 
 int main(int argc, char *argv[]){
     FILE *matrix1 = fopen(argv[1], "r");
-    printf("debug1\n");
+    //printf("debug1\n");
     int mat1width, mat1height, mat2width, mat2height;
-    fscanf(matrix1, "%d", &mat1width);//make sure its width then height
-    fscanf(matrix1, "%d", &mat1height);
-        printf("debug2\n");
+    fscanf(matrix1, "%d", &mat1height);//make sure its width then height
+    fscanf(matrix1, "%d", &mat1width);
+        //printf("debug2\n");
 
-    int *mat1 = (int *)malloc(mat1width *mat1height * sizeof(int));
+    int **mat1 = (int **)malloc(mat1height * sizeof(int*));
+    for (int i = 0; i < mat1height; i++){
+        mat1[i] = malloc(mat1width * sizeof(int));
+    }
     for (int i = 0; i < mat1height; i++){
         for (int j = 0; j < mat1width; j++){
             fscanf(matrix1, "%d", &mat1[i][j]);
         }
     }
-        printf("debug3\n");
+        //printf("debug3\n");
 
     fclose(matrix1);
     FILE *matrix2 = fopen(argv[2], "r");
-    fscanf(matrix2, "%d", &mat2width);//make sure its width then height
-    fscanf(matrix2, "%d", &mat2height);
-    int *mat2 = (int *)malloc(mat2width *mat2height * sizeof(int));
+    fscanf(matrix2, "%d", &mat2height);//make sure its width then height
+    fscanf(matrix2, "%d", &mat2width);
+    int **mat2 = (int **)malloc(mat2height * sizeof(int*));
     for (int i = 0; i < mat2height; i++){
+        mat2[i] = malloc(mat2width * sizeof(int));
+    }    for (int i = 0; i < mat2height; i++){
         for (int j = 0; j < mat2width; j++){
             fscanf(matrix1, "%d", &mat2[i][j]);
         }
     }
-        printf("debug4\n");
+        //printf("debug4\n");
 
     fclose(matrix2);
     int id = 0;
     FILE *resultMatrix = fopen(argv[3], "w");
     //add some mathy mumbo jumbo.. mat1height * mat2 width?
-    int *result = (int *)malloc(mat1width * mat2height *sizeof(int));
+    int **result = (int **)malloc(mat1height * sizeof(int*));
+    for (int i = 0; i < mat1height; i++){
+        result[i] = malloc(mat2width * sizeof(int));
+    }
     //here we figure out how to package the stuff
-    for (int i = 0; i < mat1width; i++){
-        for (int j = 0; j < mat2height; j++){
+    for (int i = 0; i < mat1height; i++){
+        for (int j = 0; j < mat2width; j++){
+
             //TODO: mathy math
-            populate(row, col, inner, data[inner*2], id);
+            //ugh i need paper
+            int data[mat1width*2];
+            //i wonder if there are notebooks for sale downstairs
+            for (int k = 0; k < mat1width; k++){
+                //i think i have my widths and heights mixed up
+                //ill fix that momentarily
+                data[k] = mat1[i][k];
+                data[k+mat1width] = mat2[k][j];
+            }
+            populate(i, j, mat1width, data, id);
             id++;
         }
     }
-        for (int i = 0; i < mat1width*mat2height; i++){
+        for (int i = 0; i < mat1height*mat2width; i++){
             recieve(result);
             }
-        printf("debug6\n");
-    fprintf(resultMatrix, "%d ",mat1width);
-    fprintf(resultMatrix, "%d\n", mat2height);
-    for (int i = 0; i < mat1width; i++){
-        for (int j = 0; j < mat2height; j++){
+        //printf("debug6\n");
+    //fprintf(resultMatrix, "%d ",mat1height);
+    //fprintf(resultMatrix, "%d\n", mat2width);
+    for (int i = 0; i < mat1height; i++){
+        for (int j = 0; j < mat2width; j++){
             fprintf(resultMatrix, "%d ", result[i][j]);
         }
     }
